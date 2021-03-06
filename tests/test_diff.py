efaedf4357878dfe5680ad851bcc700c3c010fdf
ivan-ivan-t <65672290+ivan-ivan-t@users.noreply.github.com>
2021-03-06 15:01:19 +0300

    Update test_diff.py

--- tests/test_diff.py
+++ tests/test_diff.py
@@ -4,9 +4,9 @@ import json
 
 
 def test_diff_json():
-    file1 = './tests/fixtures/first.json'
-    file2 = './tests/fixtures/second.json'
-    with open('./tests/fixtures/answer_json1.txt', 'r') as answer:
+    file1 = 'tests/fixtures/first.json'
+    file2 = 'tests/fixtures/second.json'
+    with open('tests/fixtures/answer_json1.txt', 'r') as answer:
         expected = answer.read()
     result = gendiff.generate_diff(file1, file2)
     assert result == expected
